ad8ae7d84ae9ea326b1043ebd6d832d9ac6908c8
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-06-27 16:34:18 -0500

    progress

--- calculator/SceneDelegate.swift
+++ calculator/SceneDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// It's not clear to me why I need to define this class,
+// but the app doesn't run correctly without it.
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 }
